e4fabe3bd5cc53c27be17b3a36b2b61d18996c95
kwatanabe <kwatanabe@opendoor.co.jp>
2024-06-17 13:54:56 +0900

    changed: add nginx cgi environments files for backend-frameworks environment variables eg)APP_ENV for laravel, CI_ENVIRONMENT for codeigniter & added logging to sample application for log collection tests.

--- webapp/app/Controllers/Test.php
+++ webapp/app/Controllers/Test.php
@@ -6,6 +6,9 @@ class Test extends BaseController
 {
     public function index(): string
     {
+        log_message('error', 'this is error level, Test::index() called');
+        log_message('debug', 'this is debug level, Test::index() called');
+        log_message('info', 'this is info level, Test::index() called');
         return view('test_page1');
     }
 }
